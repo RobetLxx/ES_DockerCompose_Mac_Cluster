09a8e624f1e458e303313da27e48804c78765cf0
ling <847587959@qq.com>
2025-03-04 12:51:53 +0800

    修改启动脚本只开两个 node，减少电脑开销

--- docker-es-cluster-up.sh
+++ docker-es-cluster-up.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-# 由于我本机内存只有 32G，分配给 5 个ES 实例每个 2G 的话就是 10G 没了，所以只开2个，只开两个的集群状态就为 yellow，如果配置够全开就是 green。
+# 由于我本机内存只有 32G，分配给 5 个ES 实例每个 2G 的话就是 10G 没了，所以只开2个.
 cd docker-es-master && docker-compose up -d && \
 cd ../docker-es-data01 && docker-compose up -d && \
 cd ../docker-es-data02 && docker-compose up -d && \
